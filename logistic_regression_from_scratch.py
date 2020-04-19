00ef4dcdfe5fb26800541a3665aecdac80d874e7
YF <robinycfang@gmail.com>
2020-04-19 23:52:02 +0800

    Update logistic_regression_from_scratch.py

--- logistic_regression_from_scratch.py
+++ logistic_regression_from_scratch.py
@@ -18,5 +18,50 @@ Has no header
   11. Class:                        (2 for benign, 4 for malignant)
 '''
 data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
-df = np.array(pd.read_csv(data_url, sep = ',', header = None, index_col = 0))
-print(df)
+# replace ? with Na and drop the rows with Na
+df = pd.read_csv(data_url, sep = ',', header = None, index_col = 0).replace('?', np.nan).dropna()
+df = df.astype('int64')
+# convert benign to 0 and malignant to 1
+df[10] = df[10].mask(df[10] == 2, 0)
+df[10] = df[10].mask(df[10] == 4, 1)
+df = np.array(df)
+
+# add bias to the data frame
+m_total = df.shape[0]
+bias = np.ones((m_total, 1))
+df = np.append(bias, df, axis = 1)
+
+# split the data (80% training and 20% test)
+np.random.shuffle(df)
+# m = number of training sample, n = number of features (include the bias)
+m = int(0.8 * m_total)
+n = df.shape[1] - 1
+
+x_train = df[:m, :-1]
+# y is a flatten array, so needs to be transformed into an m*1 vector
+y_train = df[:m, -1].reshape((m, 1))
+
+x_test = df[m:, :-1]
+y_test = df[m:, -1].reshape((m_total - m, 1))
+
+# set up parameters
+alpha = 0.0005	# learning rate
+iterations = 100
+theta = np.ones((n, 1))
+
+
+
+# train the logistic regression with stochastic gradient descent
+for iteration in range(iterations):
+	for i in range(m):
+		z = np.dot(x_train[i], theta)
+		h = 1 / (1 + np.exp(-z))
+		# J = -1/m * sum over m [(y * log(h) + (1 - y) * log(1 - h))], where m is 1 here
+		J = - (y_train[i] * np.log(h) + (1 - y_train[i]) * np.log(1 - h))
+
+		print('theta:')
+		print(theta)
+		print('cost: ' + str(J))
+
+		# theta -= alpha * sum over m [(h - y) * x], where m is 1 here
+		theta -= alpha * (h - y_train[i]) * x_train[i].reshape((n, -1))
